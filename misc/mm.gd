c0e2a2b44ee9f0bde1debfd581a815ba46f852bb
delilah <dont.lose.this.one.ok@gmail.com>
2026-07-06 03:35:50 +0930

    hosting working

--- misc/mm.gd
+++ misc/mm.gd
@@ -9,10 +9,10 @@ const port = 8890
 
 func _on_button_pressed() -> void:
 	self.hide()
-	Noray.connect_to_host("localhost", port)
+	await Noray.connect_to_host("html-pk.with.playit.plus", 1025)
 	Noray.register_host()
-	await Noray.register_remote(port)
-	peer.create_server(Noray.local_port)
+	Noray.register_remote(1041)
+	peer.create_server(8890)
 	multiplayer.multiplayer_peer = peer
 	add_player(multiplayer.get_unique_id())
 	Noray.on_connect_nat.connect(add_player)
@@ -21,12 +21,14 @@ func add_player(peer_id):
 	var player = PS.instantiate()
 	player.name = str(peer_id)
 	get_tree().root.get_node("/root/lobby").add_child(player)
+	print("player add")
 
 
 func _on_button_2_pressed() -> void:
 	self.hide()
-	Noray.connect_to_host($VBoxContainer/LineEdit.text, port)
-	Noray.register_host()
+	Noray.connect_nat($VBoxContainer/LineEdit.text)
 	Noray.register_remote()
+	Noray.register_host()
+	
 	peer.create_client($VBoxContainer/LineEdit.text, Noray.local_port)
 	multiplayer.multiplayer_peer = peer
